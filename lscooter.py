8697232a8d77262dd73fa31b5752f935a7e75bf2
Matthias Steinig <leblitzdick@gmail.com>
2019-01-25 01:26:37 +0100

    Add files via upload

---
+++ lscooter.py
@@ -0,0 +1,205 @@
+#!/usr/bin/python
+##
+#
+# lightning-scooter : An e-scooter whose electrical support can be booked for a certain period and paid with lightning.
+#
+#   @filename       :   lscooter.py
+#   @brief          :   Main routine
+#   @author         :   Matthias Steinig
+#
+#   Folders
+#          img      :   basic Images to display
+#          tmp      :   where the composed pictures are
+#
+#   lscooter.py main program started from /etc/rc.local
+##
+import epd2in7
+import os
+import socket
+import time
+import json, requests
+import subprocess
+from PIL import Image
+import RPi.GPIO as GPIO
+
+epd = epd2in7.EPD()
+epd.init()
+
+amount = 250000
+
+#server IP and api token must fit your settings  
+charge_url = "https://api-token:somepassword@xxx.yyy.zzz:0000"
+
+# the 4 keys on the e-paper 2,7
+#light blue
+key1 = 5
+#brown
+key2 = 6
+#light blue with black stripe
+key3 = 13
+#brown with black stripe
+key4 = 19
+
+# power switch relais
+relais18 = 18
+
+#tested server for pppd connection
+REMOTE_SERVER = "xxx.yyy.zzz"
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+# 255: clear the image with white
+image = Image.new('1', (epd2in7.EPD_WIDTH, epd2in7.EPD_HEIGHT), 255) 
+basedir = os.path.dirname(os.path.realpath(__file__))
+
+
+def lightning_getInvoice(id):
+      resp = requests.get(charge_url+'/invoice/'+id)
+#debug
+#      print(resp.json())
+      return resp.json()
+
+
+def lightning_createInvoice(amount, description):
+      invoice_details = {"msatoshi": amount, "description": "{}".format(description) }
+#debug
+#      print(invoice_details)
+      resp = requests.post(charge_url+'/invoice/', json=invoice_details)
+#debug
+#      print(resp.json())
+      return resp.json()
+            
+
+def callSubprocess(cmd):
+             subprocess.call(cmd.split())
+             
+
+def Invoice_paid(amount,boost_time):
+
+    timer = 0
+    invoice = lightning_createInvoice(amount, boost_time)
+
+    callSubprocess('qrencode -s 5 -o '+basedir+'/tmp/qr.png ' + invoice['payreq'])
+    callSubprocess('python '+basedir+'/show.py')
+
+    # Wait 60s for payment 
+    while timer < 60:
+          print(timer)
+          timer += 1
+          invoice_status = lightning_getInvoice(invoice['id'])
+          if invoice_status['status'] == 'paid':
+                return True
+          time.sleep(1)
+          
+
+# switch the time with BOOOOST
+def relaisTime(int):
+    GPIO.setup(relais18, GPIO.OUT)
+    GPIO.output(relais18, GPIO.LOW)
+    time.sleep(int)
+    GPIO.output(relais18, GPIO.HIGH)
+
+# testing for valid Internet connection     
+def is_connected(hostname):
+  try:
+    # see if we can resolve the host name -- tells us if there is               
+    # a DNS listening                                                           
+    host = socket.gethostbyname(hostname)
+    # connect to the host -- tells us if the host is actually                   
+    # reachable                                                                 
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+    pass
+  return False
+    
+
+#start GSM Internet connection  
+def startPppd():
+  os.system("/usr/sbin/pppd call gprs >> /dev/null&")
+  print "nach pppd"
+  while is_connected(REMOTE_SERVER) != True:
+    time.sleep(0.5)
+
+
+#stop GSM Internet connection  
+def stopPppd():
+    os.system("/usr/bin/killall pppd")
+
+    
+
+# display "Welcome to lightning scooter" 
+def displayWelcomeScreen():
+    epd.display_frame(epd.get_frame_buffer( Image.open(''+basedir+'/img/welcome_176x264.bmp')))
+
+    
+# display "Enjoy your ride" 
+def enjoyYourRide():
+    epd.display_frame(epd.get_frame_buffer( Image.open(''+basedir+'/img/eyr_176x264.bmp')))
+         
+
+def main():
+
+    welcome = True
+        
+    while True:
+        key1state = GPIO.input(key1)
+        key2state = GPIO.input(key2)
+        key3state = GPIO.input(key3)
+        key4state = GPIO.input(key4)
+
+        # reduce CPU usage
+        time.sleep(0.005) 
+
+        if welcome == True:
+            displayWelcomeScreen()
+            welcome = False
+        
+        if key1state == False:
+              print('Key 1min pressed')
+              os.system("/usr/bin/python /root/lscooter/streamr.py") 
+              if Invoice_paid(amount,'1min BOOOST' ) == True:
+                    stopPppd()
+                    enjoyYourRide()
+                    relaisTime(60*1)
+              displayWelcomeScreen()
+              stopPppd()
+              
+        if key2state == False:
+              print('Key 3min pressed')
+              os.system("/usr/bin/python /root/lscooter/streamr.py") 
+              if Invoice_paid((amount*3),'3min BOOOST' ) == True:
+                    stopPppd()
+                    enjoyYourRide()
+                    relaisTime(60*3)
+              displayWelcomeScreen()
+              stopPppd()
+            
+        if key3state == False:
+              print('Key 5min pressed')
+              os.system("/usr/bin/python /root/lscooter/streamr.py") 
+              if Invoice_paid((amount*5),'5min BOOOST' ) == True:
+                    stopPppd()
+                    enjoyYourRide()
+                    relaisTime(60*5)
+              displayWelcomeScreen()
+              stopPppd()
+            
+        if key4state == False:
+              print('Key cancel pressed')
+# free ride for 2 hours :-)
+              relaisTime(60*120)
+              enjoyYourRide()
+#              displayWelcomeScreen()
+              welcome = False
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ streamr.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# Filename: test-pppd.py
+
+import serial
+import time
+import os
+import subprocess
+import requests
+import json
+import time
+import socket
+import pynmea2
+
+from gattlib import GATTRequester, GATTResponse
+from struct import *
+from time import sleep
+from binascii import hexlify
+
+gpsData = ""
+
+REMOTE_SERVER = "triton739.server4you.de"
+
+streamr_stream =  {
+                  "timestamp"                : "date",
+                  "longitude"                : 0.0,
+                  "latitude"                 : 0.0,
+                  "distance_left"            : "km",
+                  "current_speed"            : "km/h",
+                  "average_speed"            : "km/h",
+                  "battery_capacity"         : "mAh",
+                  "battery_percentage"       : "%",
+                  "battery_current"          : "A",
+                  "battery_voltage"          : "V",
+                  "battery_temperature_1"    : "C",
+                  "battery_temperature_2"    : "C",
+                  "total_distance"           : "km", 
+                  "frame_temperature"        : "C"
+                  }
+
+
+
+serialPort = serial.Serial("/dev/serial0", 115200, timeout=2)
+
+
+def is_connected(hostname):
+  try:
+    # see if we can resolve the host name -- tells us if there is
+    # a DNS listening
+    host = socket.gethostbyname(hostname)
+    # connect to the host -- tells us if the host is actually
+    # reachable
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+    pass
+  return False
+
+
+def startPppd():
+  os.system("/usr/sbin/pppd call gprs >> /dev/null&")
+  print "nach pppd"
+  while is_connected(REMOTE_SERVER) != True:
+    time.sleep(0.5)
+
+  
+def killppd():
+  os.system("/usr/bin/killall pppd")
+
+  
+def uploadToStreamr():
+
+  url     = "https://www.streamr.com/api/v1/streams/FypPjVmYQKWQ2MOxwUBBhA/data"
+  payload = json.dumps(streamr_stream)
+  
+  headers = {
+    'Authorization': "token KolOfsdCSWa_oFDDZCo1TgmqZK7SuFROa--P1_A3syjQ",
+    'cache-control': "no-cache",
+    'Postman-Token': "c06734cc-f143-4b6c-b873-a63d66770898"
+  }
+  response = requests.request("POST", url, data=payload, headers=headers)
+
+ 
+def insert_Timestamp_to_streamr_stream():
+  streamr_stream["timestamp"]=time.strftime("%d.%m.%Y %H:%M:%S")
+
+def parseGPS(gps):
+  if gps.find('GGA') > 0:
+    msg = pynmea2.parse(gps)
+
+#    print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp,msg.latitude,msg.lat_dir,msg.longitude,msg.lon_dir,msg.altitude,msg.altitude_units)
+
+    sleep(1) #nessessary for timing
+    streamr_stream["latitude"]=msg.latitude
+    streamr_stream["longitude"]=msg.longitude
+
+
+  
+def readBluetoothDataM365():
+
+  MASTER_TO_M365 = 0x20
+  M365_TO_MASTER = 0x23
+  BATTERY_TO_MASTER = 0x25
+  
+  DISTANCE_INFO = 0x25
+  TRIP_INFO = 0xb0
+  BATTERY_INFO = 0x31
+
+  class Requester(GATTRequester):
+    def on_notification(self, handle, data):
+      payload = data[3:]
+      # print hexlify(payload)
+      if len(payload) > 8:
+        data_type, address = unpack('<xxxBxB', payload[:6])
+        if data_type == M365_TO_MASTER:
+          if address == DISTANCE_INFO:
+            distance_left = unpack('<H', payload[6:8])
+            print 'Distance left:', distance_left[0] / 100., 'km'
+            streamr_stream["distance_left"]= str(distance_left[0] / 100.) + ' km'
+          elif address == TRIP_INFO:
+            error, warning, flags, workmode, battery, speed, speed_average = unpack('<HHHHHHH', payload[6:20])
+            print 'Current speed:', speed / 100., 'km/h'
+            streamr_stream["current_speed"]= str(speed / 100.) + ' km/h'
+            print 'Average speed:', speed_average / 100., 'km/h'
+            streamr_stream["average_speed"]= str(speed_average / 100.) + ' km/h'
+        elif data_type == BATTERY_TO_MASTER:
+          if address == BATTERY_INFO:
+            capacity_left, battery_percent, current, voltage, battery_temperature1, battery_temperature2 \
+              = unpack('<HHhHBB', payload[6:16])
+            print 'Battery capacity:', capacity_left, 'mAh'
+            streamr_stream["battery_capacity"]= str(capacity_left) + ' mAh'
+            print 'Battery percentage:', battery_percent, '%'
+            streamr_stream["battery_percentage"]= str(battery_percent) + ' %'
+            print 'Battery current:', current / 100., 'A'
+            streamr_stream["battery_current"]= str(current / 100.) + ' A'
+            print 'Battery voltage:', voltage / 100., 'V'
+            streamr_stream["battery_voltage"]= str(voltage / 100.) + ' V'
+            print 'Battery temperature 1:', battery_temperature1 - 20, 'C'
+            streamr_stream["battery_temperature_1"]= str(battery_temperature1 - 20) + ' C'
+            print 'Battery temperature 2:', battery_temperature2 - 20, 'C'
+            streamr_stream["battery_temperature_2"]= str(battery_temperature2 - 20) + ' C'
+        else:
+          # this is a second packet from the 0xb0 request
+          total_m, temperature = unpack('<Ixxxxh', payload[:10])
+          print 'Total distance:', total_m / 1000., 'km'
+          streamr_stream["total_distance"]= str(total_m / 1000.) + ' km'
+          print 'Frame temperature', temperature / 10., 'C'
+          streamr_stream["frame_temperature"]= str(temperature / 10.) +' C'
+
+
+  #my OnePlus 5T
+  #address = "94:65:2D:C2:A5:DD"
+  #address = "AA:BB:CC:DD:EE:FF"
+  address = "FE:BF:7C:5C:B7:89"
+  requester = Requester(address, False)
+  
+  try:
+    requester.connect(True, 'random')
+  except RuntimeError as e:
+    # gattlib.connect's `wait=True` requires elevated permission
+    # or modified capabilities.
+    # It still connects, but a RuntimeError is raised. Check if
+    # `self.gatt` is connected, and rethrow exception otherwise.
+    if not requester.is_connected():
+      raise e
+    
+  # subscribe to the UART service
+  requester.write_by_handle(0x000c, str(bytearray([0x01, 0x00])))
+  sleep(0.1)
+  # speed, distance and lots of other stuff
+  requester.write_by_handle(0x000e, str(bytearray.fromhex("55aa 03 2001 b0 20 0bff")))
+  sleep(0.1)
+  # km left
+  requester.write_by_handle(0x000e, str(bytearray.fromhex("55aa 03 2001 25 02 b4ff")))
+  sleep(0.1)
+  # battery info
+  requester.write_by_handle(0x000e, str(bytearray.fromhex("55aa 03 2201 31 0a 9eff")))
+  sleep(0.1)
+
+
+    
+      
+try:
+  while True:
+    readBluetoothDataM365()
+    readBluetoothDataM365()
+    ser = serialPort.readline()
+    parseGPS(ser)
+    insert_Timestamp_to_streamr_stream()
+    startPppd()
+    uploadToStreamr()
+#    killppd()
+    print "everything is fine :-)"
+    serialPort.close()
+    break
+  
+except:
+  if serialPort != None:
+    serialPort.close()
+    
+    
+    
